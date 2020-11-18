33a6e3cf36808e0c4b0bbcb360662a5df8e95ccb
han1202012 <octopus_truth@163.com>
2020-11-18 20:33:47 +0800

    update

--- multiple-dex-tools/src/main/java/kim/hsl/multiple_dex_tools/AES.kt
+++ multiple-dex-tools/src/main/java/kim/hsl/multiple_dex_tools/AES.kt
@@ -1,5 +1,8 @@
 package kim.hsl.multiple_dex_tools
 
+import java.io.File
+import java.io.FileOutputStream
+import java.util.zip.ZipFile
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
 
@@ -77,7 +80,94 @@ fun main() {
         路径下的 multiple-dex-core-debug.aar 文件后缀修改为 .zip
         解压上述文件
         拿到 classes.jar 文件即可 ;
-
      */
 
+    // 获取 multiple-dex-core-debug.aar 文件对象
+    var aarFile = File("multiple-dex-core/build/outputs/aar/multiple-dex-core-debug.aar")
+
+    // 解压上述 multiple-dex-core-debug.aar 文件到 aarUnzip 目录中
+    // 创建解压目录
+    var aarUnzip = File("multiple-dex-tools/aarUnzip")
+    // 解压操作
+    unZipAar(aarFile, aarUnzip)
+
+    // 拿到 multiple-dex-core-debug.aar 中解压出来的 classes.jar 文件
+    var classesJarFile = File(aarUnzip, "classes.jar")
+
+    /*
+        将 jar 包变成 dex 文件 
+        使用 dx 工具命令
+        00:08:44
+     */
+    Runtime.getRuntime().exec("")
+
+
+
+
+
+}
+
+/**
+ * 删除文件, 如果有目录, 则递归删除
+ */
+private fun deleteFile(file: File) {
+    if (file.isDirectory) {
+        val files = file.listFiles()
+        for (f in files) {
+            deleteFile(f)
+        }
+    } else {
+        file.delete()
+    }
+}
+
+/**
+ * 解压文件
+ * @param zip 被解压的压缩包文件
+ * @param dir 解压后的文件存放目录
+ */
+fun unZipAar(zip: File, dir: File) {
+    try {
+        // 如果存放文件目录存在, 删除该目录
+        deleteFile(dir)
+        // 获取 zip 压缩包文件
+        val zipFile = ZipFile(zip)
+        // 获取 zip 压缩包中每一个文件条目
+        val entries = zipFile.entries()
+        // 遍历压缩包中的文件
+        while (entries.hasMoreElements()) {
+            val zipEntry = entries.nextElement()
+            // zip 压缩包中的文件名称 或 目录名称
+            val name = zipEntry.name
+            // 如果 apk 压缩包中含有以下文件 , 这些文件是 V1 签名文件保存目录 , 不需要解压 , 跳过即可
+            if (name == "META-INF/CERT.RSA" || name == "META-INF/CERT.SF" || (name
+                        == "META-INF/MANIFEST.MF")
+            ) {
+                continue
+            }
+            // 如果该文件条目 , 不是目录 , 说明就是文件
+            if (!zipEntry.isDirectory) {
+                val file = File(dir, name)
+                // 创建目录
+                if (!file.parentFile.exists()) {
+                    file.parentFile.mkdirs()
+                }
+                // 向刚才创建的目录中写出文件
+                val fileOutputStream = FileOutputStream(file)
+                val inputStream = zipFile.getInputStream(zipEntry)
+                val buffer = ByteArray(1024)
+                var len: Int
+                while (inputStream.read(buffer).also { len = it } != -1) {
+                    fileOutputStream.write(buffer, 0, len)
+                }
+                inputStream.close()
+                fileOutputStream.close()
+            }
+        }
+
+        // 关闭 zip 文件
+        zipFile.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
