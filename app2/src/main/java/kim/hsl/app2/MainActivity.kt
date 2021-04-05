f415b03cf0763b13f1f05eab173cc55457054c40
han1202012 <octopus_truth@163.com>
2021-04-05 19:44:02 +0800

    update

--- app2/src/main/java/kim/hsl/app2/MainActivity.kt
+++ app2/src/main/java/kim/hsl/app2/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 测试 ContentProvider
         contentResolver.delete(
             Uri.parse("content://kim.hsl.dex.myprovider"),
             null,
